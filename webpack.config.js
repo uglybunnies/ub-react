e9ef8e9c5b945b5802edb511757c1199247ffec8
Michael Wong <michael.wong@creditkarma.com>
2016-03-08 15:01:58 -0800

    starting over

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-
-module.exports = {
-    entry: path.resolve(__dirname, 'dev/main.js'),
-    output: {
-        path: path.resolve(__dirname, 'ub-static'),
-        filename: 'bundle.js',
-    },
-};
