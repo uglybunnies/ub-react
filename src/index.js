32f0c902f8466b5809a3cad11794011c23401ee4
Michael Wong <michael.wong@creditkarma.com>
2017-11-10 15:39:42 -0800

    updated styles

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 module.exports = function render (locals, callback) {
   callback(null, `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Ugly Bunnies</title>
-        <link rel="stylesheet" href="/style.css">
-      </head>
-      <body>
-          <header class="masthead pb">
-              <div class="logo">
-                  <h1 class="site-name s1"><span>Ugly</span> <span>Bunnies</span></h1>
-              </div>
-              <a href="#" id="menu-control" class="pa2">menu</a>
-          </header>
-          <nav class="topnav mtn">
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Ugly Bunnies</title>
+        <link href='https://fonts.googleapis.com/css?family=Henny+Penny&text=Ugly%20Bunnies|Alegreya+Sans:300,300italic,500,500italic' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header class="masthead pb">
+            <div class="logo">
+              <h1 class="site-name s1"><span>Ugly</span> <span>Bunnies</span></h1>
+            </div>
+            <a href="#" id="menu-control" class="pa2">menu</a>
+        </header>
+        <nav class="topnav mtn">
             <ul id="topNav" class="menu">
                 <li class="menuItem1"><a href="">Design</a></li>
                 <li class="menuItem2"><a href="">Art</a></li>
                 <li class="menuItem3"><a href="">Articles</a></li>
             </ul>
         </nav>
-      </body>
-    </html>
+    </body>
+</html>
   `)
 }
