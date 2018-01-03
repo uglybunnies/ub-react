d0830f47058503dbe98e648436f4c63252c82245
Michael Wong <michael@uglybunnies.com>
2018-01-03 09:51:14 -0800

    added grid styles

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ module.exports = function render (locals, callback) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Ugly Bunnies</title>
-        <link href='https://fonts.googleapis.com/css?family=Henny+Penny&text=Ugly%20Bunnies|Alegreya+Sans:300,300italic,500,500italic' rel='stylesheet' type='text/css'>
         <style>
         @font-face {
               font-family: 'fontello';
